4fd5141eb9c865210bbe1049167c06ffe5db0103
Marcelo Boschi <marceloboschi78@hotmail.com>
2019-08-21 20:21:51 -0300

    Using Exceptions

---
+++ ExerciseException/ExerciseException/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ExerciseException.Entities.Exceptions
+{
+    class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message)
+        {
+        }
+    }
+}
---
+++ ExerciseException/ExerciseException/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+using ExerciseException.Entities;
+using ExerciseException.Entities.Exceptions;
+
+namespace ExerciseException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter account data");
+                Console.Write("Number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Holder: ");
+                string holder = Console.ReadLine();
+                Console.Write("Initial balance: ");
+                double iBalance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Withdraw limit: ");
+                double wLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Account acc = new Account(number, holder, iBalance, wLimit);
+
+                Console.WriteLine();
+                Console.Write("Enter amount for withdraw: ");
+                double withdraw = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                acc.Withdraw(withdraw);
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
