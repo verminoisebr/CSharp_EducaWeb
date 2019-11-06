0fa1c02a2ffdc77abf3346ab856e740fdecdcebe
Marcelo Boschi <marceloboschi78@hotmail.com>
2019-11-06 19:19:13 -0200

    Exercise using interface

---
+++ ExerciseInterface/ExerciseInterface/Services/IOnlinePaymentService.cs
@@ -0,0 +1,8 @@
+﻿namespace ExerciseInterface.Services
+{
+    interface IOnlinePaymentService
+    {
+        double PaymentFee(double amount);
+        double Interest(double amount, int months);
+    }
+}
